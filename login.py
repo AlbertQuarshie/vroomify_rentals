db4726e448ae4b646aa25eb939119067542a8411
AlbertQuarshie <162796723+AlbertQuarshie@users.noreply.github.com>
2026-05-12 09:45:56 +0300

    Creating the login page

---
+++ login.py
@@ -0,0 +1,58 @@
+import customtkinter as ctk
+from tkinter import messagebox
+from mongodb import users_collection
+
+
+class LoginApp(ctk.CTk):
+
+    def __init__(self):
+        super().__init__()
+
+        self.title("Vroomify Rentals - Login")
+        self.geometry("400x450")
+
+        # Title
+        self.title_label = ctk.CTkLabel(self, text="Login", font=("Arial", 24))
+        self.title_label.pack(pady=20)
+
+        # Username
+        self.username_entry = ctk.CTkEntry(self, placeholder_text="Username")
+        self.username_entry.pack(pady=10)
+
+        # Password
+        self.password_entry = ctk.CTkEntry(self, placeholder_text="Password", show="*")
+        self.password_entry.pack(pady=10)
+
+        # Login Button
+        self.login_btn = ctk.CTkButton(self, text="Login", command=self.login)
+        self.login_btn.pack(pady=20)
+
+    # =========================
+    # LOGIN FUNCTION
+    # =========================
+    def login(self):
+
+        username = self.username_entry.get()
+        password = self.password_entry.get()
+
+        if username == "" or password == "":
+            messagebox.showerror("Error", "All fields are required")
+            return
+
+        # Check user in database
+        user = users_collection.find_one({
+            "username": username,
+            "password": password
+        })
+
+        if user:
+            role = user.get("role", "user")
+
+            messagebox.showinfo("Success", "Login successful")
+
+            self.destroy()
+
+            
+
+        else:
+            messagebox.showerror("Error", "Invalid username or password")
